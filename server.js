a348c06bed6f8c6d932454ffb8c9d76505f54460
George Arnold <george.arnold.88@gmail.com>
2020-01-31 07:29:53 -0500

    getting server ready for heroku

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const helmet = require("helmet");
 const MOVIES = require("./movies-data-small.json");
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
@@ -41,7 +42,7 @@ app.get("/movie", (req, res) => {
 
   res.json(response);
 });
-const PORT = 8000;
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
