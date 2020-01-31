360a5fecd7d22241f0bf3d9a7978169e682b509c
George Arnold <george.arnold.88@gmail.com>
2020-01-31 07:35:04 -0500

    preparing for heroku

--- server.js
+++ server.js
@@ -42,6 +42,16 @@ app.get("/movie", (req, res) => {
 
   res.json(response);
 });
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
